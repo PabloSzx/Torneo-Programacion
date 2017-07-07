7693bda3c8a5a22fdbce249e52899f769c1b4c7b
Pablo Sáez Parra <pablosaez1995@gmail.com>
2017-07-07 17:13:40 -0400

    buscaminas terminado

--- BuscaMinas/Buscaminas.java
+++ BuscaMinas/Buscaminas.java
@@ -2,9 +2,48 @@ import java.util.Scanner;
 
 class Buscaminas {
   char[][] campo;
-  
+
+  int[][] calcular() {
+    int[][] pn = new int[this.campo.length][this.campo[0].length];
+    for (int i= 0; i < this.campo.length; i++){
+    	for (int j = 0; j < this.campo[i].length; j++){
+    		if (this.campo[i][j] == '*'){
+    			pn[i][j] = -999;
+          if (validar(i, j - 1)) {
+            pn[i][j-1]++;
+          }
+          if (validar(i, j + 1)) {
+            pn[i][j+1]++;
+          }
+          if (validar(i + 1, j)) {
+            pn[i+1][j]++;
+          }
+          if (validar(i - 1, j)) {
+            pn[i-1][j]++;
+          }
+          if (validar(i - 1, j - 1)) {
+            pn[i-1][j-1]++;
+          }
+          if (validar(i - 1, j + 1)) {
+            pn[i-1][j+1]++;
+          }
+          if (validar(i + 1, j - 1)) {
+            pn[i+1][j-1]++;
+          }
+          if (validar(i + 1, j + 1)) {
+            pn[i+1][j+1]++;
+          }
+    		}
+    	}
+    }
+
+    return pn;
+  }
+
+
   boolean validar(int a, int b) {
-    if (this.campo.length < a && this.campo[0].length < b) {
+
+    if (this.campo.length > a && a >= 0  && b >= 0 &&  this.campo[0].length > b) {
       return true;
     }
     return false;
@@ -17,23 +56,47 @@ class Buscaminas {
       System.out.println();
     }
   }
-  public static void main(String[] args) {
-    Buscaminas zona = new Buscaminas();
-    String linea;
-    Scanner keyboard = new Scanner(System.in);
-    int n = keyboard.nextInt();
-    int m = keyboard.nextInt();
-    zona.campo = new char[n][m];
-    for (int c = 0; c < n; c++) {
-      linea = keyboard.next();
-      for (int i = 0; i < m; i++) {
-        zona.campo[c][i] = linea.charAt(i);
+
+  void printCampo(int[][] matriz) {
+    for (int i = 0; i < matriz.length; i++) {
+      for (int j = 0; j < matriz[i].length; j++) {
+        if (matriz[i][j] >= 0) {
+          System.out.print(matriz[i][j]);
+        } else {
+          System.out.print("*");
+        }
+        if (j < matriz[i].length) {
+          System.out.print(" ");
+        }
       }
+      System.out.println();
     }
+  }
+  public static void main(String[] args) {
+    int contador = 0;
 
-    System.out.println("Campo insertado");
+    while (true) {
+      Buscaminas zona = new Buscaminas();
+      String linea;
+      Scanner keyboard = new Scanner(System.in);
+      int n = keyboard.nextInt();
+      int m = keyboard.nextInt();
+      zona.campo = new char[n][m];
+      for (int c = 0; c < n; c++) {
+        linea = keyboard.next();
+        for (int i = 0; i < m; i++) {
+          zona.campo[c][i] = linea.charAt(i);
+        }
+      }
 
-    zona.printCampo();
+      System.out.print("\n\nField #");
+      System.out.print(contador);
+      System.out.print("\n");
+
+      int[][] aux = new int[n][m];
+      aux = zona.calcular();
+      zona.printCampo(aux);
+    }
   }
 
 
