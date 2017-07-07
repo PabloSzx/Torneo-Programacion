2ac5a7d2a89511a8a011f981c230d8676906549b
Pablo Sáez Parra <pablosaez1995@gmail.com>
2017-07-07 16:33:07 -0400

    buscaminas

---
+++ BuscaMinas/Buscaminas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Buscaminas {
+  String[][] campo;
+  public static void main(String[] args) {
+    Buscaminas 
+    Scanner keyboard = new Scanner(System.in);
+    int n = keyboard.nextInt();
+    int m = keyboard.nextInt();
+    campo = new String[n][m];
+    for (int c = 0; c < n; c++) {
+      String linea = keyboard.nextLine();
+      for (int i = 0; i < m; i++) {
+
+      }
+    }
+  }
+}
