721460b4df175fcc07b9b0f5d4a7d0fce39a738b
Pablo Sáez Parra <pablosaez1995@gmail.com>
2017-07-07 16:51:58 -0400

    avance

--- BuscaMinas/Buscaminas.java
+++ BuscaMinas/Buscaminas.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
 
 class Buscaminas {
-  String[][] campo;
+  char[][] campo;
+  
+  boolean validar(int a, int b) {
+    if (this.campo.length < a && this.campo[0].length < b) {
+      return true;
+    }
+    return false;
+  }
+  void printCampo() {
+    for (int i = 0; i < campo.length; i++) {
+      for (int j = 0; j < campo[i].length; j++) {
+        System.out.print(campo[i][j]);
+      }
+      System.out.println();
+    }
+  }
   public static void main(String[] args) {
-    Buscaminas 
+    Buscaminas zona = new Buscaminas();
+    String linea;
     Scanner keyboard = new Scanner(System.in);
     int n = keyboard.nextInt();
     int m = keyboard.nextInt();
-    campo = new String[n][m];
+    zona.campo = new char[n][m];
     for (int c = 0; c < n; c++) {
-      String linea = keyboard.nextLine();
+      linea = keyboard.next();
       for (int i = 0; i < m; i++) {
-
+        zona.campo[c][i] = linea.charAt(i);
       }
     }
+
+    System.out.println("Campo insertado");
+
+    zona.printCampo();
   }
+
+
 }
