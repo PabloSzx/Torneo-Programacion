c43fec30b563693aa770bff26c55dd7e7a758c1d
Diego Sandoval <sandov@protonmail.com>
2017-08-09 16:32:04 -0400

    asdfas

--- File-Fragmentation/Files.java
+++ File-Fragmentation/Files.java
@@ -3,17 +3,24 @@ import java.util.*;
 public class Files{
 	public static void main(String[] args){
 		Scanner teclado = new Scanner(System.in);
-
 		String input;
+		ArrayList<String> lineas = new ArrayList<String>();
+		int n = 0;
+
+		n = teclado.nextInt();
+		teclado.nextLine();
 
 		do {
-				input = teclado.nextLine();
-			
+				input = teclado.nextLine().trim();
+				lineas.add(input);
 
-		} while (input != "");
+
+		} while (!(input.equals("")));
 
 		System.out.println("Ended");
-		
+
+		System.out.println("lineas: " + lineas + "    " + n );
+
 
 	}
 
