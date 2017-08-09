260012fedd4a2acd5ffd01b70807a19d9adca9fe
Diego Sandoval <sandov@protonmail.com>
2017-08-09 15:21:49 -0400

    Empezando 3.8.6

--- File-Fragmentation/Files.java
+++ File-Fragmentation/Files.java
@@ -3,11 +3,21 @@ import java.util.*;
 public class Files{
 	public static void main(String[] args){
 		Scanner teclado = new Scanner(System.in);
+
+		String input;
+
+		do {
+				input = teclado.nextLine();
+			
+
+		} while (input != "");
+
+		System.out.println("Ended");
 		
 
 	}
 
 	public String procesar(){
-		
+		return "";
 	}
 }
